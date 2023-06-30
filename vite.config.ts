0b41899fb720a5fc1015b2867928d65f969d7393
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-06-30 13:02:23 +0300

    init: set up project

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+    },
+  },
+  plugins: [svgr(), react()],
+  base: '/effective-happiness',
+});
